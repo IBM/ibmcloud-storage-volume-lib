c16d7926e54d7c4f0b1c36d56cf7238432d73b84
arahamad <arahamad@in.ibm.com>
2019-03-28 15:49:46 +0530

    updated copyright date for vpc

--- volume-providers/vpc/vpcclient/models/constants.go
+++ volume-providers/vpc/vpcclient/models/constants.go
@@ -2,7 +2,7 @@
  * IBM Confidential
  * OCO Source Materials
  * IBM Cloud Container Service, 5737-D43
- * (C) Copyright IBM Corp. 2019 All Rights Reserved.
+ * (C) Copyright IBM Corp. 2018, 2019 All Rights Reserved.
  * The source code for this program is not  published or otherwise divested of
  * its trade secrets, irrespective of what has been deposited with
  * the U.S. Copyright Office.
--- volume-providers/vpc/vpcclient/vpcvolume/volume_service.go
+++ volume-providers/vpc/vpcclient/vpcvolume/volume_service.go
@@ -2,7 +2,7 @@
  * IBM Confidential
  * OCO Source Materials
  * IBM Cloud Container Service, 5737-D43
- * (C) Copyright IBM Corp. 2018 All Rights Reserved.
+ * (C) Copyright IBM Corp. 2018, 2019 All Rights Reserved.
  * The source code for this program is not  published or otherwise divested of
  * its trade secrets, irrespective of what has been deposited with
  * the U.S. Copyright Office.
@@ -19,9 +19,6 @@ import (
 type VolumeManager interface {
 	// Create the volume with authorisation by passing required information in the volume object
 	CreateVolume(*models.Volume) (*models.Volume, error)
-
-	// Create the volume from snapshot with snapshot tags
-	//CreateVolumeFromSnapshot(snapshot models.Snapshot, tags map[string]string) (*modelsVolume, error)
 
 	// Delete the volume
 	DeleteVolume(volumeID string) error
