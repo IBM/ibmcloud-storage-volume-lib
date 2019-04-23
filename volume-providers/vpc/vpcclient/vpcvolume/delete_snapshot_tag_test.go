87900ef255a55dabefe778a401812b035670b4a2
Narasimha Karumanchi <narkarum@in.ibm.com>
2019-04-23 18:23:20 +0530

    VPC UT changes

---
+++ volume-providers/vpc/vpcclient/vpcvolume/delete_snapshot_tag_test.go
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ * IBM Confidential
+ * OCO Source Materials
+ * IBM Cloud Container Service, 5737-D43
+ * (C) Copyright IBM Corp. 2018 All Rights Reserved.
+ * The source code for this program is not  published or otherwise divested of
+ * its trade secrets, irrespective of what has been deposited with
+ * the U.S. Copyright Office.
+ ******************************************************************************/
+
+package vpcvolume_test
+
+import (
+	"github.com/IBM/ibmcloud-storage-volume-lib/volume-providers/vpc/vpcclient/models"
+	"github.com/IBM/ibmcloud-storage-volume-lib/volume-providers/vpc/vpcclient/riaas/test"
+	"github.com/IBM/ibmcloud-storage-volume-lib/volume-providers/vpc/vpcclient/vpcvolume"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
+)
+
+func TestDeleteSnapshotTag(t *testing.T) {
+	// Setup new style zap logger
+	logger, _ := GetTestContextLogger()
+	defer logger.Sync()
+
+	testCases := []struct {
+		name string
+
+		// Response
+		status  int
+		content string
+
+		// Expected return
+		expectErr string
+		verify    func(*testing.T, *models.Snapshot, error)
+	}{
+		{
+			name:   "Verify that the correct endpoint is invoked",
+			status: http.StatusNoContent,
+		}, {
+			name:      "Verify that a 404 is returned to the caller",
+			status:    http.StatusNotFound,
+			content:   "{\"errors\":[{\"message\":\"testerr\"}]}",
+			expectErr: "Trace Code:, testerr Please check ",
+		}, {
+			name:    "Verify that the snapshot is parsed correctly",
+			status:  http.StatusOK,
+			content: "{\"id\":\"snapshot1\",\"name\":\"snapshot1\",\"status\":\"pending\"}",
+			verify: func(t *testing.T, snapshot *models.Snapshot, err error) {
+				if assert.NotNil(t, snapshot) {
+					assert.Equal(t, "snapshot1", snapshot.ID)
+				}
+			},
+		},
+	}
+
+	for _, testcase := range testCases {
+		t.Run(testcase.name, func(t *testing.T) {
+			mux, client, teardown := test.SetupServer(t)
+			emptyString := ""
+			test.SetupMuxResponse(t, mux, "volumes/volume-id/snapshots/snapshotid/tags/tag-name", http.MethodDelete, &emptyString, testcase.status, testcase.content, nil)
+
+			defer teardown()
+
+			snapshotService := vpcvolume.NewSnapshotManager(client)
+
+			err := snapshotService.DeleteSnapshotTag("volume-id", "snapshot-id", "tag-name", logger)
+			// vpc snapshot functionality is not yet ready. It would return error for now
+			assert.Error(t, err)
+		})
+	}
+}
---
+++ volume-providers/vpc/vpcclient/vpcvolume/list_volume_tags_test.go
@@ -0,0 +1,71 @@
+/*******************************************************************************
+ * IBM Confidential
+ * OCO Source Materials
+ * IBM Cloud Container Service, 5737-D43
+ * (C) Copyright IBM Corp. 2018 All Rights Reserved.
+ * The source code for this program is not  published or otherwise divested of
+ * its trade secrets, irrespective of what has been deposited with
+ * the U.S. Copyright Office.
+ ******************************************************************************/
+
+package vpcvolume_test
+
+import (
+	"github.com/IBM/ibmcloud-storage-volume-lib/volume-providers/vpc/vpcclient/models"
+	"github.com/IBM/ibmcloud-storage-volume-lib/volume-providers/vpc/vpcclient/riaas/test"
+	"github.com/IBM/ibmcloud-storage-volume-lib/volume-providers/vpc/vpcclient/vpcvolume"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+	"net/http"
+	"testing"
+)
+
+func TestListVolumeTags(t *testing.T) {
+	// Setup new style zap logger
+	logger, _ := GetTestContextLogger()
+	defer logger.Sync()
+
+	testCases := []struct {
+		name string
+
+		// Response
+		status  int
+		content string
+
+		// Expected return
+		expectErr string
+		verify    func(*testing.T, *models.Volume, error)
+	}{
+		{
+			name:   "Verify that the correct endpoint is invoked",
+			status: http.StatusNoContent,
+		}, {
+			name:      "Verify that a 404 is returned to the caller",
+			status:    http.StatusNotFound,
+			content:   "{\"errors\":[{\"message\":\"testerr\"}]}",
+			expectErr: "Trace Code:, testerr Please check ",
+		},
+	}
+
+	for _, testcase := range testCases {
+		t.Run(testcase.name, func(t *testing.T) {
+			mux, client, teardown := test.SetupServer(t)
+			test.SetupMuxResponse(t, mux, "/volumes/volume-id/tags", http.MethodGet, nil, testcase.status, testcase.content, nil)
+
+			defer teardown()
+
+			volumeService := vpcvolume.New(client)
+
+			volumeTags, err := volumeService.ListVolumeTags("volume-id", logger)
+			logger.Info("volumeTags", zap.Reflect("volumeTags", volumeTags))
+
+			if testcase.expectErr != "" && assert.Error(t, err) {
+				assert.Equal(t, testcase.expectErr, err.Error())
+				assert.Nil(t, volumeTags)
+			} else {
+				assert.NoError(t, err)
+				assert.NotNil(t, volumeTags)
+			}
+		})
+	}
+}
