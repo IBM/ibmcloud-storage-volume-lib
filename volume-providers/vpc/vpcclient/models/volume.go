77f721fde66fd00e36db0c9029a17350b991e184
Narasimha Karumanchi <narkarum@in.ibm.com>
2019-05-21 14:06:07 +0530

    volume encrytption

--- volume-providers/vpc/vpcclient/models/volume.go
+++ volume-providers/vpc/vpcclient/models/volume.go
@@ -14,19 +14,20 @@ import "time"
 
 // Volume ...
 type Volume struct {
-	Href              string              `json:"href,omitempty"`
-	ID                string              `json:"id,omitempty"`
-	Name              string              `json:"name,omitempty"`
-	Capacity          int64               `json:"capacity,omitempty"`
-	Iops              int64               `json:"iops,omitempty"`
-	ResourceGroup     *ResourceGroup      `json:"resource_group,omitempty"`
-	Tags              []string            `json:"tags,omitempty"`
-	Generation        GenerationType      `json:"generation,omitempty"`
-	Profile           *Profile            `json:"profile,omitempty"`
-	Snapshot          *Snapshot           `json:"snapshot,omitempty"`
-	CreatedAt         *time.Time          `json:"created_at,omitempty"`
-	Status            StatusType          `json:"status,omitempty"`
-	VolumeAttachments *[]VolumeAttachment `json:"volume_attachments,omitempty"`
+	Href                string               `json:"href,omitempty"`
+	ID                  string               `json:"id,omitempty"`
+	Name                string               `json:"name,omitempty"`
+	Capacity            int64                `json:"capacity,omitempty"`
+	Iops                int64                `json:"iops,omitempty"`
+	VolumeEncryptionKey *VolumeEncryptionKey `json:"encryption_key,omitempty"`
+	ResourceGroup       *ResourceGroup       `json:"resource_group,omitempty"`
+	Tags                []string             `json:"tags,omitempty"`
+	Generation          GenerationType       `json:"generation,omitempty"`
+	Profile             *Profile             `json:"profile,omitempty"`
+	Snapshot            *Snapshot            `json:"snapshot,omitempty"`
+	CreatedAt           *time.Time           `json:"created_at,omitempty"`
+	Status              StatusType           `json:"status,omitempty"`
+	VolumeAttachments   *[]VolumeAttachment  `json:"volume_attachments,omitempty"`
 
 	Zone *Zone  `json:"zone,omitempty"`
 	CRN  string `json:"crn,omitempty"`
