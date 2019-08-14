1176fd7455fd3b31db4c146f496b1b5af47e6577
Narasimha Karumanchi <narkarum@in.ibm.com>
2019-08-14 13:15:11 +0530

    e2e

--- e2e/scripts/send-messages-to-slack.py
+++ e2e/scripts/send-messages-to-slack.py
@@ -4,21 +4,21 @@ import os
 import sys
 from slackclient import SlackClient
 
-slack_client = SlackClient('SLACK_TOKEN_REPLACE')
-
+slack_client = SlackClient(token=os.environ['SLACK_API_TOKEN'])
+channelName = "ibm-vpc-block-csi-e2e"
 def channel_info(channel_id):
-    channel_info = slack_client.api_call("channels.info", channel=channel_id)
-    if channel_info:
-        return channel_info['channel']
+    channels_info = slack_client.api_call("channels.info", channel=channel_id)
+    if channels_info:
+        return channels_info
     return None
 
 def send_file (FilePath):
     gopath = os.environ["GOPATH"]
     PathToFile = gopath + "/src/github.com/IBM/ibmcloud-storage-volume-lib/" + FilePath
-
+    print PathToFile
     slack_client.api_call(
         "files.upload",
-        channel="storage-test-runs",
+        channel=channelName,
         file=(FilePath, open(PathToFile, 'rb'), 'txt'),
         filename=FilePath,
         username='IBM VPC Storage',
@@ -27,19 +27,23 @@ def send_file (FilePath):
 def send_message (FilePath):
     gopath = os.environ["GOPATH"]
     PathToFile = gopath + "/src/github.com/IBM/ibmcloud-storage-volume-lib/" + FilePath
-
+    if slack_client:
+        print channel_info(channelName)
+        print PathToFile
+    else:
+        print "Client is None"
     with open(PathToFile, 'r') as content_file:
         content = content_file.read()
 
     slack_client.api_call(
         "chat.postMessage",
-        channel="storage-test-runs",
+        channel=channelName,
         text=content,
         username='IBM VPC storage common library e2e test results')
 
 if __name__ == '__main__':
-    filePath = sys.argv[2]
-    isAttachment = sys.argv[3]
+    filePath = sys.argv[1]
+    isAttachment = sys.argv[2]
     if filePath is None or isAttachment is None:
         print "Please send the variables filePath and isAttachment"
         sys.exit(1)
